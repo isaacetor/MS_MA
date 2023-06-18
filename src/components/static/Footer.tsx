f37f1af117286d59990216b274c8e164bf1485c6
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-06-18 22:14:56 +0100

    added the footer

--- src/components/static/Footer.tsx
+++ src/components/static/Footer.tsx
@@ -1,6 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="w-full h-[50px] fixed bottom-0 z-10 border-t-[1px] bg-white"></footer>
+    <footer className="w-full h-[50px] fixed bottom-0 z-10 border-t-[1px] bg-white flex items-center">
+      <div className="w-[90%] m-[auto] text-sm">
+        <p>© {new Date().getFullYear()} MSMA</p>
+      </div>
+    </footer>
   );
 };
 
