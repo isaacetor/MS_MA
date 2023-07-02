d4334596b0ca86f3ce9861168ae029ec05a07ac0
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-07-02 23:58:16 +0100

    working on profile page

--- src/components/props/GlobalButton.tsx
+++ src/components/props/GlobalButton.tsx
@@ -5,7 +5,7 @@ const GlobalButton: FC<buttonData> = ({ text }) => {
   return (
     <div>
       <div>
-        <button className="py-[10px] px-[15px] border-[1px] border-gray-400  rounded-md font-bold text-[18px] hover:bg-gray-100 transition-all ease-in-out duration-200 max-md:text-sm">
+        <button className="py-[10px] px-[15px] border-[1px] border-gray-400  rounded-md font-bold hover:bg-gray-100 transition-all ease-in-out duration-200 max-md:text-sm">
           {text}
         </button>
       </div>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,7 +4,7 @@ import UserPostBlock from "../components/blocks/profile/UserPostBlock";
 const Profile = () => {
   return (
     <div>
-      <div className="w-[90%] m-auto h-[200vh] my-4 flex gap-14 max-lg:block">
+      <div className="w-[90%] m-auto py-8 my-4 flex gap-14 max-lg:block max-md:pt-1">
         {/* profile */}
         <UserBlock />
 
